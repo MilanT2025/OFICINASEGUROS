8d12b4e5fa9f155f9f8d5ce3f5ef2eb6594ef522
ejmg3 <ejmg3@DESKTOP-PTJ4GM6>
2024-10-26 09:51:57 -0500

    Inicio de un sistema de asistencia se ha creado conexion a la bd y creacion de formulario

---
+++ src/main/java/vista/ConexionBD.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ejmg3
+ */
+public class ConexionBD {
+    // Conexion base de datos de servidor Galenos
+    static Connection cnn = null;
+
+    static String usuario = "sa";
+    static String contraseña = "123456";
+    static String bd = "HOSPITAL_SIS";
+    static String ip = "192.168.10.114";
+    static String puerto = "1433;"+"encrypt=true;trustServerCertificate=true";
+
+    String cadena = "jdbc:sqlserver://"+ip+";"+puerto+"/"+bd;
+
+    public static Connection establecerConexion() {
+        try {
+            String cadena = "jdbc:sqlserver://localhost\\SQLEXPRESS:"+puerto+";"+"databaseName="+bd;
+            cnn = DriverManager.getConnection(cadena,usuario,contraseña);
+//            JOptionPane.showMessageDialog(null, "Se conecto correctamente a la base de datos");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar a la base de datos, error: " + e.toString());
+        }
+        return cnn;
+    }
+}
